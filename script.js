1aeaa54432f23c982b38259d56815db0d7a1be7e
Milind611 <singhmilind222@gmail.com>
2025-10-01 14:33:11 +0530

    made it responsive

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
-const inputBox=document.getElementById("input-box");
-const listContainer=document.getElementById("list-container");
-function addTask()
-{
-    if(inputBox.value==='')
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const addBtn = document.getElementById("add-btn");
+
+addBtn.addEventListener("click", addTask);
+
+function addTask() {
+    if (inputBox.value.trim() === '') {
         alert("You must write something!");
-    else{
-        let li=document.createElement("li");
-        li.innerHTML=inputBox.value;
+        return;
+    } else {
+        let li = document.createElement("li");
+        li.textContent = inputBox.value;   
         listContainer.appendChild(li);
-        let span=document.createElement("span");
-        span.innerHTML="\u00d7";
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
-    inputBox.value="";
+    inputBox.value = "";
     saveData();
 }
-listContainer.addEventListener("click",function(e)
-{
-    if(e.target.tagName==="LI")
-    {
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         saveData();
-    }
-    else if(e.target.tagName==="SPAN")
-    {
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
     }
 });
-function saveData()
-{
-    localStorage.setItem("data",listContainer.innerHTML);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
 }
-function showTask()
-{
-    listContainer.innerHTML=localStorage.getItem("data");
+
+function showTask() {
+    const data = localStorage.getItem("data");
+    if (data) {
+        listContainer.innerHTML = data;
+    }
 }
+
 showTask();
